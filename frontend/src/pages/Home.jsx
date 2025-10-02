1981a95530e003a2c6a68fc43d5da1f65553546d
Gimhani <silvagsd@students.nsbm.ac.lk>
2025-10-02 23:11:46 +0530

    Refactor Home component to use NoteCard for rendering notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,9 @@ import { useState } from 'react';
 import RateLimitedUI from '../components/RateLimitedUI';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { PenSquareIcon, Trash2Icon } from 'lucide-react';
+import { formatDate } from '../lib/utils';
+import NoteCard from '../components/NoteCard';
 
 const Home = () => {
     
@@ -37,10 +40,7 @@ const Home = () => {
       {notes.length > 0 && (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {notes.map(note => (
-                <div key={note._id} className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-primary border-solid p-4 rounded-lg shadow-md'>
-                    <h3 className='text-lg font-semibold mb-2'>{note.title}</h3>
-                    <p className='text-gray-600'>{note.content}</p>
-                </div>
+                <NoteCard key={note._id} note={note} />
             ))}
         </div>
       )}
